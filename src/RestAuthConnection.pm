4f7892f29dcefa73f35a3320d690f39edc863875
Mathias Ertl <mati@fsinf.at>
2012-05-05 21:25:50 +0200

    more improvements

--- src/RestAuthConnection.pm
+++ src/RestAuthConnection.pm
@@ -7,7 +7,6 @@ sub new {
     my $class = shift;
     my $self = {
         _url => shift,
-        _path => shift,
     };
     bless $self, $class;
 
@@ -29,19 +28,23 @@ sub set_credentials {
 
 sub set_content_handler {
     my $self = shift;
+    $self->{_mime} = shift;
 }
 
 sub request {
     # parameters:
     # * curl handler
-    # * headers
+    # * path
     my $self = shift;
     my $curl = shift;
+    my $path = shift;
+    
+    $curl->setopt(WWW::Curl::Share::CURLOPT_URL(), $self->{_url} . $path);
 
     #TODO: use correct content-type header
     my @headers = (
         "Authorization: $self->{_auth_header}",
-        'Accept: application/json',
+        "Accept: $self->{_mime}",
     );
 
     $curl->setopt(WWW::Curl::Share::CURLOPT_HTTPHEADER(), \@headers, 1);
@@ -66,34 +69,41 @@ sub curl_handler {
 
     my $curl = new WWW::Curl::Easy;
     $curl->setopt(WWW::Curl::Share::CURLOPT_HEADER(), 1);
-    $curl->setopt(WWW::Curl::Share::CURLOPT_URL(), $self->{_url});
     $curl->setopt(WWW::Curl::Share::CURLOPT_TIMEOUT(), 2);
     return $curl;
 }
 
 sub get {
     my $self = shift;
+    my $path = shift;
     my $curl = $self->curl_handler;
     
-    my $response = $self->request($curl);
+    my $response = $self->request($curl, $path);
     my $response_code = $curl->getinfo(WWW::Curl::Share::CURLINFO_HTTP_CODE());
-    print("Response code: $response_code");
+    return $curl;
 }
 
 sub post {
     my $self = shift;
-    my $headers = $self->get_headers;
+    my $path = shift;
     
     my $curl = $self->curl_handler;
     $self->request($curl);
+    return $curl;
 }
 
 sub put {
     my $self = shift;
+    my $curl = $self->curl_handler;
+    my $path = shift;
+    return $curl;
 }
 
 sub delete {
     my $self = shift;
+    my $curl = $self->curl_handler;
+    my $path = shift;
+    return $curl;
 }
 1;
 
@@ -103,9 +113,28 @@ package RestAuthContentHandler;
 package RestAuthResource;
 sub new {
     my $class = shift;
-    my $self = {};
+    my $self = {
+        _conn => shift,
+    };
 
     bless $self, $class;
     return $self;
 }
+
+sub request_get {
+    my $self = shift;
+    my $path = $self->prefix . shift;
+    
+    return $self->{_conn}->get($path);
+}
+
+sub prefix {
+    my $self = shift;
+    my $class = ref($self) || $self;
+    my $varname = $class . "::prefix";
+    no strict 'refs';
+    print $$varname, "\n";
+    return $$varname;
+}
+
 1;
