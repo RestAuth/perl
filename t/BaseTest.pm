ee6513259a164d2edae0ebf366ad84251be3a92a
Mathias Ertl <mati@fsinf.at>
2012-05-17 11:47:35 +0200

    correctly remove all users at the start of each test

--- t/BaseTest.pm
+++ t/BaseTest.pm
@@ -5,5 +5,10 @@ use RestAuthConnection;
 sub set_up {
     my $self = shift;
     $self->{conn} = new RestAuthConnection('http://[::1]:8000', 'example.com', 'example');
+    
+    my @users = RestAuthUser->get_all($self->{conn});
+    for my $user (@users) {
+        $user->remove();
+    }
 }
 1;
