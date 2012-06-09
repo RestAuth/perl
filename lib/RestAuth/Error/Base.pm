ae9100cd190b9ad6c4e17cb2d9bb99c4ac325b16
Mathias Ertl <mati@fsinf.at>
2012-06-09 10:33:01 +0200

    add complete POD documentation

--- lib/RestAuth/Error/Base.pm
+++ lib/RestAuth/Error/Base.pm
@@ -13,10 +13,46 @@
 # You should have received a copy of the GNU General Public License
 # along with perl-RestAuth.  If not, see <http://www.gnu.org/licenses/>.
 
+=head1 NAME
+
+RestAuth::Error::Base - Superclass for all RestAuth related exceptions
+
+=head1 DESCRIPTION
+
+This class is a superclass for all exceptions related to RestAuth.
+
+=head1 INHERITANCE
+
+=over
+
+=item *
+
+B<Subclasses:> L<RestAuth::Error::Http>
+
+=back
+
+=head1 METHODS
+
+=cut
 package RestAuth::Error::Base;
 use base qw(Error);
 use overload ('""' => 'stringify');
 
+=head2 new($text)
+
+Basic constructor.
+
+PARAMETERS
+
+=over
+
+=item *
+
+B<text> - string - A human-readable text indicating the error.
+
+=back
+
+=cut
 sub new {
     my $self = shift;
     my $text = "" . shift;
@@ -27,4 +63,25 @@ sub new {
 
     $self->SUPER::new(-text => $text, @args);
 }
+
+
+=head1 BUGS
+
+To report bugs in this library please either join our RestAuth XMPP channel
+found at restauth@conference.jabber.at or file an issue in L<our bugtracker
+|https://redmine.fsinf.at/projects/restauth-perl>.
+
+=head1 LICENSE
+
+Copyright 2012, Mathias Ertl L<mati@restauth.net|mailto:mati@restauth.net>
+
+This software is free. It is licensed under the
+L<GNU General Public License, version 3|http://www.gnu.org/copyleft/gpl.html>.
+
+The latest version of this software should be available via
+L<our git repository|https://git.fsinf.at/restauth/perl> or via this projects
+homepage, L<perl.restauth.net|https://perl.restauth.net>.
+
+=cut
+
 1;
--- lib/RestAuth/Error/Http.pm
+++ lib/RestAuth/Error/Http.pm
@@ -13,12 +13,75 @@
 # You should have received a copy of the GNU General Public License
 # along with perl-RestAuth.  If not, see <http://www.gnu.org/licenses/>.
 
+=head1 NAME
+
+RestAuth::Error::Http - Superclass for all exceptions caused by HTTP responses.
+
+=head1 DESCRIPTION
+
+This class is a superclass for all exceptions thrown when doing an HTTP request
+and the server response indicates an error of some kind.
+
+=head1 INHERITANCE
+
+=over
+
+=item *
+
+B<Superclasses:> L<RestAuth::Error::Base>
+
+=item *
+
+B<Subclasses:> L<RestAuth::Error::UnsupportedMediaType>,
+L<RestAuth::Error::NotFound>, L<RestAuth::Error::Conflict>
+
+=back
+
+=head1 METHODS
+
+=cut
 package RestAuth::Error::Http;
 use base RestAuth::Error::Base;
 
+=head2 new($response)
+
+Basic constructor.
+
+PARAMETERS:
+
+=over
+
+=item *
+
+B<response> - 
+L<HTTP::Response|http://search.cpan.org/~gaas/HTTP-Message-6.03/lib/HTTP/Response.pm>
+- The response send by the server.
+
+=back
+=cut
 sub new {
     my $self = shift;
     $self->{response} = shift;
     $self->SUPER::new($self->{response}->content);
 }
+
+=head1 BUGS
+
+To report bugs in this library please either join our RestAuth XMPP channel
+found at restauth@conference.jabber.at or file an issue in L<our bugtracker
+|https://redmine.fsinf.at/projects/restauth-perl>.
+
+=head1 LICENSE
+
+Copyright 2012, Mathias Ertl L<mati@restauth.net|mailto:mati@restauth.net>
+
+This software is free. It is licensed under the
+L<GNU General Public License, version 3|http://www.gnu.org/copyleft/gpl.html>.
+
+The latest version of this software should be available via
+L<our git repository|https://git.fsinf.at/restauth/perl> or via this projects
+homepage, L<perl.restauth.net|https://perl.restauth.net>.
+
+=cut
+
 1;
