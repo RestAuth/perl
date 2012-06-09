1f064ec498957a187d6e0e9ae551edb3dfc07bc9
Mathias Ertl <mati@fsinf.at>
2012-06-09 10:33:25 +0200

    add complete POD documentation

--- lib/RestAuth/Error/Conflict.pm
+++ lib/RestAuth/Error/Conflict.pm
@@ -13,7 +13,52 @@
 # You should have received a copy of the GNU General Public License
 # along with perl-RestAuth.  If not, see <http://www.gnu.org/licenses/>.
 
+=head1 NAME
+
+RestAuth::Error::Conflict - Superclass for exceptions related to entities that
+already exist.
+
+=head1 DESCRIPTION
+
+This class is a superclass for all exceptions thrown when trying to create an
+entity (user, property, group) that already exists.
+
+=head1 INHERITANCE
+
+=over
+
+=item *
+
+B<Superclasses:> L<RestAuth::Error::Http>
+
+=item *
+
+B<Subclasses:> L<RestAuth::Error::UserExists>
+
+=back
+
+=cut
 package RestAuth::Error::Conflict;
 use base RestAuth::Error::Http;
 our $code = 409;
+
+=head1 BUGS
+
+To report bugs in this library please either join our RestAuth XMPP channel
+found at restauth@conference.jabber.at or file an issue in L<our bugtracker
+|https://redmine.fsinf.at/projects/restauth-perl>.
+
+=head1 LICENSE
+
+Copyright 2012, Mathias Ertl L<mati@restauth.net|mailto:mati@restauth.net>
+
+This software is free. It is licensed under the
+L<GNU General Public License, version 3|http://www.gnu.org/copyleft/gpl.html>.
+
+The latest version of this software should be available via
+L<our git repository|https://git.fsinf.at/restauth/perl> or via this projects
+homepage, L<perl.restauth.net|https://perl.restauth.net>.
+
+=cut
+
 1;
